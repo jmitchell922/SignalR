f0452705d2cf4337469e7472cb18c8f46263d3b8
Justin Mitchell <jmitchell922@gmail.com>
2016-11-04 21:50:38 -0400

    Initial Commit

---
+++ SignalRExample/Scripts/SignalR.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    $.connection.hub.start()
+       .done(function () {
+           console.log("Connection Established");
+           $.connection.myHub.server.announce("Connected!");
+       })
+       .fail(function () {
+           alert('Connection Failed');
+       });
+
+    $.connection.myHub.client.announce = function (message) {
+        $("#welcome-messages").append(message + "<br />");
+    }
+
+})()
