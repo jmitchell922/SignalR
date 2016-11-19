f29b87bc7678554a7abc3c3fec161efa23b669d9
Justin Mitchell <jmitchell922@gmail.com>
2016-11-19 15:26:10 -0500

    Added method and hub names.

--- SignalRExample/MyHub.cs
+++ SignalRExample/MyHub.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace SignalRExample
 {
-    public class MyHub : Hub
+    [HubName("chat")]
+    public class ChatHub : Hub
     {
+        [HubMethodName("announceToAll")]
         public void Announce(string message)
         {
             Clients.All.Announce(message);
--- SignalRExample/Scripts/SignalR.js
+++ SignalRExample/Scripts/SignalR.js
@@ -1,17 +1,25 @@
 ﻿(function () {
-    var myHub = $.connection.myHub;
+    var myHub = $.connection.chat;
+
+    $("#click-me")
+        .on("click",
+            function() {
+                myHub.server.getServerDateTime()
+                .done(function (data) {
+                    writeToPage(data);
+                })
+                .fail(function (e) {
+                    writeToPage(e);
+                });
+            });
+
+    
 
     $.connection.hub.start()
        .done(function () {
             writeToPage("Successfully Connected!");
-            myHub.server.announce("Connected!");
-            myHub.server.getServerDateTime()
-                .done(function(data) {
-                    writeToPage(data);
-                })
-                .fail(function(e) {
-                    writeToPage(e);
-                });
+            myHub.server.announceToAll("Connected!");
+            
         })
        .fail(function () {
             writeToPage("Error: Connection Failed");
